c40a2605dd4d09969365cd8e4335a81ac66ea363
Joshua Ardito <josh@Joshuas-MBP.fios-router.home>
2020-04-05 23:56:30 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+import {useEffect} from 'react';
 import './App.css';
+import {css} from 'glamor';
+import L from 'leaflet';
+
+const PARISH_JSON = require('./data/jamaica-parishes.json');
+const mapStyle = css({
+  height: '500px',
+});
+
+const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiYXJkaXRvam9zaCIsImEiOiJjazhueDEwNDQwdWt6M3JwZXAwd2hiajJuIn0.cftbrDv5zJTpODE1khs_Tw';
+const MAPBOX_TILES_URL = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}';
+const MAPBOX_ATTRIBUTION_URL ='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
+
+const TILE_SIZE = 512;
+
+const JAMAICA_LAT = 18.12;
+const JAMAICA_LONG = -77.3;
 
 function App() {
+  const map = <div id="map" className={mapStyle}></div>;
+
+  useEffect(() => {
+    const mymap = L.map('map').setView([JAMAICA_LAT, JAMAICA_LONG], 9);
+    L.tileLayer(MAPBOX_TILES_URL, {
+        attribution: MAPBOX_ATTRIBUTION_URL,
+        maxZoom: 18,
+        id: 'mapbox/light-v9',
+        tileSize: TILE_SIZE,
+        zoomOffset: -1,
+        accessToken: MAPBOX_ACCESS_TOKEN,
+    }).addTo(mymap);
+    L.geoJSON(PARISH_JSON).addTo(mymap);
+
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    Hello World!
+    {map}
     </div>
   );
 }
